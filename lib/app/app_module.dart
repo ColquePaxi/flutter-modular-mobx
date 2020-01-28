43fbae686420b09abe2ba47ef86becdf064ba83a
Colque <colque@gmail.com>
2020-01-28 16:30:00 -0300

    Aula4: Criada a splash screen, e também o módulo de LOGIN (porém o meu está no formato do mobx e não foi feita a injeção de dependência no Bind do app_module (e o router para Login também foi feito à mão). Tem que ver porque isso não está automatizado ou se realmente é assim mesmo.

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,18 +1,25 @@
+import 'package:slidy_aula_2/app/pages/splash/splash_controller.dart';
 import 'package:slidy_aula_2/app/app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:slidy_aula_2/app/app_widget.dart';
 import 'package:slidy_aula_2/app/modules/home/home_module.dart';
+import 'package:slidy_aula_2/app/pages/splash/splash_page.dart';
+
+import 'modules/login/login_module.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
+        Bind((i) => SplashController()),
         Bind((i) => AppController()),
       ];
 
   @override
   List<Router> get routers => [
-        Router('/', module: HomeModule()),
+        Router('/', child: (_, args) => SplashPage()),
+        Router('/login', module: LoginModule()),
+        Router('/home', module: HomeModule()),
       ];
 
   @override
